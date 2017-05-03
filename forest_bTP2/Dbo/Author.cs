74a140a6b085a9edf5219de5e2e68c99f320859e
francois forestier <francois.forestier@epita.fr>
2017-05-03 13:11:24 +0200

    add Dbo class

---
+++ forest_bTP2/Dbo/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace forest_bTP2.Dbo
+{
+    public class Author
+    {
+        public Author(long id, string name, string firstname)
+        {
+            this.id = id;
+            Name = name;
+            Firstname = firstname;
+
+        }
+
+        public long id { get; set; }
+        public string Name { get; set; }
+        public string Firstname { get; set; }
+        public ICollection<Book> Books { get; set; }
+
+    }
+}
---
+++ forest_bTP2/Dbo/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace forest_bTP2.Dbo
+{
+    public class Book
+    {
+        public Book(long id, string title, DateTime? publication, long idauthor)
+        {
+            Id = id;
+            Title = title;
+            Publication = publication;
+            Idauthor = idauthor;
+        }
+
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public Nullable<DateTime> Publication { get; set; }
+        public long Idauthor { get; set; }
+        
+    }
+}
