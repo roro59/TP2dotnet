98d58805e6a1918a525ac2446984ba20e89bd0ee
francois forestier <francois.forestier@epita.fr>
2017-05-03 21:38:25 +0200

    add data access

---
+++ forest_bTP2/DataAccess/Author.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace forest_bTP2.DataAccess
+{
+    public class Author : IDisposable
+    {
+        public DataAccess.ModelTP Context { get; set; }
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+        public void Initializer()
+        {
+            Context = new ModelTP();
+        }
+        public Author()
+        {
+        }
+
+        public List<Dbo.Author> ListAuthors()
+        {
+            var a = new List<Dbo.Author>();
+            var qerry = from tmp in Context.T_Author
+                select tmp;
+            qerry.ToList().ForEach(e => a.Add(new Dbo.Author(e.Name, e.Firstname)));
+            return a;
+        }
+    }
+}
---
+++ forest_bTP2/DataAccess/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace forest_bTP2.DataAccess
+{
+    public class Book
+    {
+    public Dbo.Book f()
+        {
+            return null; 
+
+        }
+    
+    }
+}
